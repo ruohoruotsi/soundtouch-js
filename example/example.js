fdd4403318e30e93c72cd03bd70df72d31fcea15
ruohoruotsi <ruoho.ruotsi@gmail.com>
2016-07-18 14:45:25 -0700

    [FIX] up repo so it works out of the box in FF with the latest audiocontext, example file and a few comments and un-minified version for debugging

--- example/example.js
+++ example/example.js
@@ -5,13 +5,13 @@ window.AudioContext = window.AudioContext ||
   window.oAudioContext ||
   window.msAudioContext
 
-var context = new webkitAudioContext();
+var context = new AudioContext();
 
 var pitchshifter, buffer;
 
 //GET AUDIO FILE
 var request = new XMLHttpRequest();
-request.open('GET', 'sound1.wav', true);
+request.open('GET', 'christina_01.wav', true);
 request.responseType = 'arraybuffer';
 
 request.onload = function() {
@@ -20,17 +20,22 @@ request.onload = function() {
         //we now have the audio data
         buffer = buf;
         console.log('decoded');
-        pitchshifter = new PitchShifter(context, buffer, 1024);
-        pitchshifter.tempo = 0.75;
+        pitchshifter = new PitchShifter(context, buffer, 64);
+        pitchshifter.tempo = 0.95;
+
+        // playback right-away upon page-refresh, we'll do this in play()
+        // console.log("default playback rate: " + pitchshifter.tempo)
+        // pitchshifter.connect(context.destination);
     });
 }
 
 console.log('reading url');
 request.send();
 
-//PLAYBACK
+// PLAYBACK
 function play() {
     pitchshifter.connect(context.destination);
+    console.log("default playback rate: " + pitchshifter.tempo)
     console.log("play")
 }
 
